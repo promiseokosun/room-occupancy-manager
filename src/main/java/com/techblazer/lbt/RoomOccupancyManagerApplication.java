771e9afe61bb870f3e00d977ae1bdda1e902c244
promise <promiseokosun@gmail.com>
2021-10-26 00:27:25 +0100

    Initial commit

---
+++ src/main/java/com/techblazer/lbt/RoomOccupancyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.techblazer.lbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancyManagerApplication.class, args);
+	}
+
+}
